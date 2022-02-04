57011436ebd0a24ae63061dea506839193aaab37
unknown <brian131198@gmail.com>
2022-02-04 16:17:41 -0500

    second commit

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
+const http = require("http")
+const port = 3000
+
+const server = http.createServer(function(req,res){
+    res.write('Hello World')
+    res.end()
+})
+
+server.listen(port, function(error){
+    if (error){
+        console.log("Errorr", error)
+    }
+    else{
+        console.log("Listening "+ port)
+    }
+})
+
+
 console.log("Hello World");
